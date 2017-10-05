2ae466d89184606dccfa332adc867943c50861f4
amithKadasur <amith.kadasur@gmail.com>
2017-10-05 17:57:01 +0200

    somemore function stuff

--- python-scraps/functions.py
+++ python-scraps/functions.py
@@ -18,3 +18,18 @@ pizza_toppings_tuple('onions', 'pepperoni', 'mushrooms')
 print_this_list = ['one', 'two', 'three']
 print(print_this_list)
 print(*print_this_list)
+
+'''using arbitrary keyword Arguments'''
+person = {}
+def build_profile(first, last, **user_info):
+    person["first_name"] = first
+    person["last_name"] = last
+    print(user_info.items())
+    print(*user_info.items())
+
+    for key, value in user_info.items():
+        person[key] = value
+    return person
+
+user_profile = build_profile("amith", "kn", location = "somewhere", quote = "liveNow")
+print(user_profile)
